87a43e48c6f8887e16572d25e875c05ec9af923e
Abrokenheart <33343600@qq.com>
2023-12-16 22:22:00 +0800

    Site updated: 2023-12-16 22:21:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/4a17b156.html"];function toRandomPost(){
+var posts=["posts/fd71581c.html","posts/4a17b156.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
